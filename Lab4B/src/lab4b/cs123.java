0c273a4611c1b4efbaa93e22ec7473aa6138eb36
JustusRC <91702048+JustusRC@users.noreply.github.com>
2021-12-15 14:12:10 -0700

    average

---
+++ Lab4B/src/lab4b/cs123.java
@@ -0,0 +1,31 @@
+package lab4b;
+
+import java.util.Scanner;
+
+public class cs123 {
+
+	public static void main(String[] args) {
+		//read user input
+		Scanner pam = new Scanner (System.in);
+	     //Variable declaration
+		int value=5, i=0, n, sum=0, average ;
+		
+		//Has enters 5 integers
+		System.out.print("Enter 5 integer number and press enter: ");
+		
+		
+		do {    //do-while loop    
+        	n=pam.nextInt();
+    		sum=sum+n;
+			i++;  
+           }
+        while (i<value);
+		//calculates average
+		average=sum/value;
+		
+		System.out.println("The average value is: "+average);
+		
+
+	}
+
+}
